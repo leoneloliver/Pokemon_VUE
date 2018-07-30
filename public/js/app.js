12b64709cf4bbed9378438112a6f4fd44e86ab58
leoneloliver <leoneloliver@gmail.com>
2018-07-30 17:22:24 -0400

    Including files

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        pokemonList: [],
+        nameFilter: ''
+    },
+    mounted: function(){
+      PokeService.API.Pokemon.listAll().then(pokemonList => {
+          this.pokemonList = pokemonList;
+      })
+    },
+    computed: {
+        pokeList: function(){
+            var nameFilter = this.nameFilter.toLowerCase();
+            return this.pokemonList.filter( pokemon => pokemon.name.includes(nameFilter) )
+        }
+    }
+})
---
+++ public/js/list-component.js
@@ -0,0 +1,17 @@
+Vue.component('my-list', {
+    template: `
+        <li class="poke-list-item" >
+            <img :src="sprite" />
+            <span>{{pokemon.number | pokeNumber}} - {{pokemon.name}}</span>
+        </li>
+    `,
+    props: ['pokemon'],
+    computed: {
+        number: function(){
+            return Vue.filter('pokeNumber')(this.pokemon.number);
+        },
+        sprite: function(){
+            return `//serebii.net/pokedex-xy/icon/${this.number}.png`
+        }
+    }
+})
